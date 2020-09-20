85e6a8603dcd156e8614036cfcba7d371494bb07
SeHee Son <seheeson@MacBook-Pro.local>
2020-09-20 02:35:31 -0700

    fixing

--- db/db.js
+++ db/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const connectionString = 'mongodb://localhost/electricasa';
-const URI = `mongodb+srv://seheesf88:mEoGo7sljHDxC3QM@cluster0.4c1d1.mongodb.net/electricasa-v3?retryWrites=true&w=majority`
+const MONGODB_URL = `mongodb+srv://seheesf88:mEoGo7sljHDxC3QM@cluster0.4c1d1.mongodb.net/electricasa-v3?retryWrites=true&w=majority`
 
-mongoose.connect(URI || process.env.MONGODB_URI || connectionString, {
+mongoose.connect(MONGODB_URL || process.env.MONGODB_URI || connectionString, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
