1e10cea5cfbc2eebb043bf120bbdb27c181c90b8
SeHee Son <seheeson@MacBook-Pro.local>
2020-12-15 00:11:04 -0800

    back

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://localhost:3000', 'https://electricasa-v3-frontend.herokuapp.com/'],
+  origin: ['http://localhost:3000', 'https://localhost:3000', 'https://electricasa-frontend.herokuapp.com/'],
   credentials: true,
   optionsSuccessStatus:200
 }
