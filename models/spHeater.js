547eb478b1c8f1671b1f165b56db80555a67553b
SeHee Son <seheeson@MacBook-Pro.local>
2021-01-12 20:38:25 -0800

    update model

--- models/spHeater.js
+++ models/spHeater.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const spHeaterSchema = mongoose.Schema({
   spHeaterImg: { type: String },
   spHeaterType: { type: String },
-  atticSqft: { type: String },
+  spHeaterBrand: { type: String },
   spHeaterYear: { type: String },
   spHeaterCondition : { type: String },
   coolingSystem : { type: String },
