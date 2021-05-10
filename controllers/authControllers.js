c5bbf80268182f0aff5b51460e965dcfdfa7cc01
Thomas Strub <thomstrub@gmail.com>
2021-05-10 15:46:40 -0700

    pulls routes into separate directory and creates controller functions for JWT

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,14 +3,16 @@ const router  = express.Router();
 const bcrypt = require('bcrypt');
 const User    = require('../models/user');
 
+const jwt = require('jsonwebtoken');
 const SECRET = process.env.SECRET;
 
-router.get('/', async(req, res) => {
+
+async function getAllUsers(req, res){
   const AllUsers = await User.find({});
   res.json({
     user: AllUsers,
   })
-})
+}
 
 //register//create
 
@@ -48,6 +50,39 @@ async function login(req, res) {
   }
 }
 
+// JWT logout is done through the client on the FE via remove token function
+
+
+// Find user for user info editing
+
+async function findUser(req, res) {
+    try {
+      const foundUser = await User.findById(req.params.id);
+  
+      res.json({
+        status: 200,
+        data: foundUser
+      });
+    } catch(err) {
+      res.send(err);
+    };
+}
+
+async function editUser(req, res){
+  try {
+
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {...req.body}, {new:true});
+
+    res.json({
+      status: 200,
+      data: 'user is updated',
+      updatedUser: updatedUser
+    });
+} catch(err) {
+  res.json(err)
+};
+}
+
 //--------------------------helper functions-----------------------//
 function createJWT(user) {
   return jwt.sign(
@@ -57,137 +92,146 @@ function createJWT(user) {
   );
 }
 
+// Get all users
+
+// router.get('/', async(req, res) => {
+//   const AllUsers = await User.find({});
+//   res.json({
+//     user: AllUsers,
+//   })
+// })
+
 //register//create
 
-router.post('/', async(req, res) => {
-  const foundUser = await User.findOne({email: req.body.email});
+// router.post('/', async(req, res) => {
+//   const foundUser = await User.findOne({email: req.body.email});
 
-  if(!foundUser){
-    const email = req.body.email;
-    const password = req.body.password;
-    const hashedPassword = await bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const phNumber = req.body.phNumber;
-    const emailNotice = req.body.emailNotice;
-    const mobileNotice = req.body.mobileNotice;
-    const UserDbEntry = {};
-          UserDbEntry.email        = email;
-          UserDbEntry.password     = hashedPassword;
-          UserDbEntry.firstName    = firstName;
-          UserDbEntry.lastName     = lastName;
-          UserDbEntry.phNumber     = phNumber;
-          UserDbEntry.emailNotice  = emailNotice;
-          UserDbEntry.mobileNotice = mobileNotice;
+//   if(!foundUser){
+//     const email = req.body.email;
+//     const password = req.body.password;
+//     const hashedPassword = await bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+//     const firstName = req.body.firstName;
+//     const lastName = req.body.lastName;
+//     const phNumber = req.body.phNumber;
+//     const emailNotice = req.body.emailNotice;
+//     const mobileNotice = req.body.mobileNotice;
+//     const UserDbEntry = {};
+//           UserDbEntry.email        = email;
+//           UserDbEntry.password     = hashedPassword;
+//           UserDbEntry.firstName    = firstName;
+//           UserDbEntry.lastName     = lastName;
+//           UserDbEntry.phNumber     = phNumber;
+//           UserDbEntry.emailNotice  = emailNotice;
+//           UserDbEntry.mobileNotice = mobileNotice;
 
-    try {
-      const user = await User.create(UserDbEntry);
+//     try {
+//       const user = await User.create(UserDbEntry);
 
-      req.session.logged = true;
-      req.session.userId = user._id;
+//       req.session.logged = true;
+//       req.session.userId = user._id;
 
-      res.json({
-        status: 200,
-        data: 'register successful',
-        userId: user._id,
-      });
+//       res.json({
+//         status: 200,
+//         data: 'register successful',
+//         userId: user._id,
+//       });
 
-    } catch(err) {
-      res.send(err);
-    };
-  } else {
-    res.send('this email is... ')
-  };
-});
+//     } catch(err) {
+//       res.send(err);
+//     };
+//   } else {
+//     res.send('this email is... ')
+//   };
+// });
 
-//login
-router.post('/login', async(req, res) => {
-  try {
-    const foundUser = await User.findOne({email: req.body.email});
-    if (foundUser) {
-      const passwordMatches = bcrypt.compareSync(req.body.password, foundUser.password);
-      if (bcrypt.compareSync(req.body.password, foundUser.password)) {
-        req.session.message = '';
-        req.session.logged = true;
-        req.session.userId = foundUser._id;
+// //login
+// router.post('/login', async(req, res) => {
+//   try {
+//     const foundUser = await User.findOne({email: req.body.email});
+//     if (foundUser) {
+//       const passwordMatches = bcrypt.compareSync(req.body.password, foundUser.password);
+//       if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+//         req.session.message = '';
+//         req.session.logged = true;
+//         req.session.userId = foundUser._id;
 
-        res.json({
-          status: 200,
-          data: 'login successful',
-          userId: foundUser._id
-        });
-      } else {
-        req.session.message = 'email or password is not correct';
+//         res.json({
+//           status: 200,
+//           data: 'login successful',
+//           userId: foundUser._id
+//         });
+//       } else {
+//         req.session.message = 'email or password is not correct';
 
-        res.status(401).json({
-          status: 401,
-          data: 'login unsuccessful'
-        });
-      };
-    } else {
-      req.session.message = 'email or password is incorrect';
+//         res.status(401).json({
+//           status: 401,
+//           data: 'login unsuccessful'
+//         });
+//       };
+//     } else {
+//       req.session.message = 'email or password is incorrect';
 
-      res.json({
-        status: 401,
-        data: 'login unsuccessful',
-      });
-    };
-  } catch(err) {
-    res.send(err)
-  };
-});
+//       res.json({
+//         status: 401,
+//         data: 'login unsuccessful',
+//       });
+//     };
+//   } catch(err) {
+//     res.send(err)
+//   };
+// });
 
-//logout
-router.get('/logout', (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json({
-        status: 200,
-        data: 'logout successful'
-      });
-    };
-  });
-});
+// //logout
+// router.get('/logout', (req, res) => {
+//   req.session.destroy((err) => {
+//     if (err) {
+//       res.send(err);
+//     } else {
+//       res.json({
+//         status: 200,
+//         data: 'logout successful'
+//       });
+//     };
+//   });
+// });
 
-router.get('/:id', async(req, res) => {
-  try {
-    const foundUser = await User.findById(req.params.id);
+// router.get('/:id', async(req, res) => {
+//   try {
+//     const foundUser = await User.findById(req.params.id);
 
-    res.json({
-      status: 200,
-      data: foundUser
-    });
-  } catch(err) {
-    res.send(err);
-  };
-});
+//     res.json({
+//       status: 200,
+//       data: foundUser
+//     });
+//   } catch(err) {
+//     res.send(err);
+//   };
+// });
 
-router.put('/:id', async(req, res) => {
-  try {
-      let modifyProfile = {};
-      const password = req.body.password;
-      const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+// router.put('/:id', async(req, res) => {
+//   try {
+//       let modifyProfile = {};
+//       const password = req.body.password;
+//       const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-      modifyProfile.email = req.body.email;
-      modifyProfile.password = hashedPassword;
-      modifyProfile.firstName = req.body.firstName;
-      modifyProfile.lastName = req.body.lastName;
-      modifyProfile.phNumber = req.body.phNumber;
-      modifyProfile.emailNotice = req.body.emailNotice;
-      modifyProfile.mobileNotice = req.body.mobileNotice;
+//       modifyProfile.email = req.body.email;
+//       modifyProfile.password = hashedPassword;
+//       modifyProfile.firstName = req.body.firstName;
+//       modifyProfile.lastName = req.body.lastName;
+//       modifyProfile.phNumber = req.body.phNumber;
+//       modifyProfile.emailNotice = req.body.emailNotice;
+//       modifyProfile.mobileNotice = req.body.mobileNotice;
 
-      const updatedUser = await User.findByIdAndUpdate(req.params.id, modifyProfile, {new:true})
+//       const updatedUser = await User.findByIdAndUpdate(req.params.id, modifyProfile, {new:true})
 
-      res.json({
-        status: 200,
-        data: 'user is updated',
-        updatedUser: updatedUser
-      });
-  } catch(err) {
-    res.json(err)
-  };
-});
+//       res.json({
+//         status: 200,
+//         data: 'user is updated',
+//         updatedUser: updatedUser
+//       });
+//   } catch(err) {
+//     res.json(err)
+//   };
+// });
 
-module.exports = router
+// module.exports = router
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const authCtrl = require('../../controllers/authControllers');
 
+router.get('/', authCtrl.getAllUsers);
 
 /*---------- Public Routes ----------*/
 router.post('/signup', authCtrl.signup);
 router.post('/login', authCtrl.login);
 
 /*---------- Protected Routes ----------*/
+router.get('/:id', authCtrl.findUser);
+router.put('/:id', authCtrl.editUser);
+
+module.exports = router
