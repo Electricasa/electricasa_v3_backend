7ad0e5c47226279f0174c5e39751a8e4c602c1f3
Thomas Strub <thomstrub@gmail.com>
2021-05-20 18:02:36 -0400

    populates all fields in address with exception of user

--- controllers/addressControllers.js
+++ controllers/addressControllers.js
@@ -8,7 +8,14 @@ module.exports = {
 }
 //posts = await PlantPost.find({}).sort({'_id': -1}).populate('user').populate('plant').exec() // userSchema.set('toObject') gets invoked, to delete the password
 async function getAllAddresses(req, res){
-    const allAddresses = await Address.find({}).sort({'._id': -1}).populate('user').populate('attic').populate('house')
-                    .populate('roof').populate('spHeater').populate('utility').populate('waHeater').exec();
-    res.json({allAddresses});
+    try{
+        const allAddresses = await Address.find({}).populate('attic').populate('house')
+                    .populate('roof').populate('spHeater').populate('utility')
+                    .populate('waHeater').exec();
+        res.status(200).json({allAddresses});
+    } catch(err){
+        return res.status(401).json(err)
+    }
+    
 }
+
