68fff5196decce9c70c880aa27e55dc568c49044
Thomas Strub <thomstrub@gmail.com>
2021-05-20 17:23:10 -0400

    address model's completed field is marked as true if all sections are added except for utility

--- utils/photoUploadService.js
+++ utils/photoUploadService.js
@@ -8,13 +8,17 @@ const { v4: uuidv4 } = require('uuid');
 const S3 = require('aws-sdk/clients/s3');
 const s3 = new S3(); // initialize the construcotr
 
-// current fields necessary for an address entry to be considered complete
-const addressFields = ["attic", "house", "roof", "spHeater", "utility", "waHeater"]
+// current fields necessary for an address entry to be considered complete 
+// utility is optional
+const addressFields = ["attic", "house", "roof", "spHeater", "waHeater"]
 
 // checks if address document has necessary fields entered to be "complete"
 function checkComplete(addressDoc){
-  for (let item in addressFields){
-    if(!addressDoc[item]){
+  for (let idx in addressFields){
+    // console.log(addressDoc, "addressDoc from checkComplete");
+    // console.log(idx, "idx from checkComplete");
+    // console.log(addressDoc[addressFields[idx]], "m--------W Keys of addressDoc")
+    if(!addressDoc[addressFields[idx]]){
       return false;
     }
   }
